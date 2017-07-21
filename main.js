9dc89d748e2984a030880e8ab92d186bf7dfdc09
Alexandre Escudero <aescudero@sqli.com>
2017-07-21 09:54:28 +0200

    Practice 03 - Solution

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 {
-    const persons = [{firstName: 'Robin', lastName: 'Coma Delperier'}, {firstName: 'Matthieu', lastName: 'Mauny'}, {firstName: 'Alex', lastName: 'Escudero'}];
+    function Person(firstName, lastName) {
+        Object.assign(this, {
+            firstName,
+            lastName
+        });
+    }
+    Object.assign(Person.prototype, {
+        fullName() {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        getIn() {
+            console.log('Getting in the door');
+        },
+        ringBell(times) {
+            new Array(times).fill().forEach((value, index) => console.log(`Ringing the bell #${index + 1}`));
+        }
+    });
+
+    const persons = [{firstName: 'Robin', lastName: 'Coma Delperier'}, {firstName: 'Matthieu', lastName: 'Mauny'}, {firstName: 'Alex', lastName: 'Escudero'}]
+        .map(attributes => new Person(attributes.firstName, attributes.lastName));
 
     function Greeter(messagePrefix, messageSuffix) {
         this.prefix = messagePrefix;
@@ -7,11 +26,12 @@
     }
     Greeter.prototype.welcomePersons = function (persons) {
         return persons.map((person) => {
-            return `${this.prefix}<em>${person.firstName} ${person.lastName}</em>${this.suffix}`;
+            return `${this.prefix}<em>${person.fullName()}</em>${this.suffix}`;
         });
     };
 
     const greeter = new Greeter('Greetings, kind ', ', have a nice day');
     const greetings = greeter.welcomePersons(persons);
     console.log(greetings);
+    console.log(persons);
 }
